f81151bd3f44a373ae22979534486fe17549dad1
Neels Hofmeyr <neels@hofmeyr.de>
2017-07-18 17:56:48 +0200

    iudummy: remove public ranap_ue_conn_ctx_alloc
    This function has been removed in osmo-iuh.

--- tests/libiudummy/iudummy.c
+++ tests/libiudummy/iudummy.c
@@ -63,9 +63,3 @@ int ranap_iu_tx_release(struct ranap_ue_conn_ctx *ctx, const struct RANAP_Cause 
 	LOGP(DLGLOBAL, LOGL_INFO, "iu_tx_release() dummy called, NOT sending Release\n");
 	return 0;
 }
-
-struct ranap_ue_conn_ctx *ranap_ue_conn_ctx_alloc(struct osmo_sccp_addr *addr, uint32_t conn_id)
-{
-	LOGP(DLGLOBAL, LOGL_INFO, "ue_conn_ctx_alloc() dummy called, NOT allocating UE context\n");
-	return NULL;
-}
