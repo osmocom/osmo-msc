fe6acd61897ce977697825792d59909b4e816273
Philipp Maier <pmaier@sysmocom.de>
2017-12-05 13:02:37 +0100

    mncc: re-add lchan members to structs
    The lchan related struct members do not serve any useful purpose
    in the msc code, since the lchan concept is not in the scope of
    osmo-msc. However, if removed te struct size will change which
    will lead into shortened protocol messages as well. This is
    is detected by osmo-sip-connector and eventually leads into
    a reject ofthe shortended protocol messages.
    
    Re add the missing struct members in order to maintain
    compatibility
    
    This commit reverts the changes made to mncc.h by commit:
    e2f24d53e4f80b34ec6d656d93127cb598229a96
    
    Change-Id: Ia02373a36df7605507ee3de49173a9fd6547b726

--- include/osmocom/msc/mncc.h
+++ include/osmocom/msc/mncc.h
@@ -155,6 +155,9 @@ struct gsm_mncc {
 	int		notify; /* 0..127 */
 	int		emergency;
 	char		imsi[16];
+
+	unsigned char	lchan_type;
+	unsigned char	lchan_mode;
 };
 
 struct gsm_data_frame {
@@ -176,6 +179,7 @@ struct gsm_mncc_hello {
 	uint32_t	called_offset;
 	uint32_t	signal_offset;
 	uint32_t	emergency_offset;
+	uint32_t	lchan_type_offset;
 };
 
 struct gsm_mncc_rtp {
