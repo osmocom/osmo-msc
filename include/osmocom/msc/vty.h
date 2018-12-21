osmocom/osmo-msc

commit 1051c4208834c22d344dd92d25addfd8e5d95993

require 'ipa-name' option to be set via config file
The 'ipa-name' option can now only be set via the configuration file
because changing the IPA name at run-time conflicts with active
GSUP connections and routes configured in the HLR. The osmo-msc
program must be restarted if its IPA name needs to change.

Change-Id: I6cff91793e646e0396e8f1bc87d0f52709e5f12a
Related: OS#3355

include/osmocom/msc/vty.h
@@ -31,4 +31,6 @@ void msc_vty_init(struct gsm_network *msc_network);
 
 struct gsm_network *gsmnet_from_vty(struct vty *vty);
 
+extern bool msc_parsing_config_file;
+
 #endif

src/libmsc/msc_vty.c
@@ -466,6 +466,12 @@ DEFUN(cfg_msc_ipa_name,
       "This name is used for GSUP routing and must be set if more than one MSC is connected to the HLR. "
       "The default is 'MSC-00-00-00-00-00-00'.\n")
 {
+	if (!msc_parsing_config_file) {
+		vty_out(vty, "The IPA name cannot be changed at run-time; "
+			"It can only be set in the configuraton file.%s", VTY_NEWLINE);
+		return CMD_WARNING;
+	}
+
 	gsmnet->msc_ipa_name = talloc_strdup(gsmnet, argv[0]);
 	return CMD_SUCCESS;
 }

src/osmo-msc/msc_main.c
@@ -24,6 +24,7 @@
  *
  */
 
+#include <stdbool.h>
 #include <unistd.h>
 #include <time.h>
 #include <errno.h>
@@ -514,6 +515,8 @@ extern void *tall_gsms_ctx;
 extern void *tall_call_ctx;
 extern void *tall_trans_ctx;
 
+bool msc_parsing_config_file = false;
+
 int main(int argc, char **argv)
 {
 	int rc;
@@ -566,7 +569,9 @@ int main(int argc, char **argv)
 		return -1;
 #endif
 
+	msc_parsing_config_file = true;
 	rc = vty_read_config_file(msc_cmdline_config.config_file, NULL);
+	msc_parsing_config_file = false;
 	if (rc < 0) {
 		LOGP(DMSC, LOGL_FATAL, "Failed to parse the config file: '%s'\n",
 		     msc_cmdline_config.config_file);

commit b361ea7037a306621a0308a35c700dc1d76b7f11

use vty->type instead of local variable
We can check if we're parsing the config file by checking
whether vty->type equals VTY_FILE. This avoids the use of
an extra local variable to track the parsing state.

Change-Id: I85161575e025f7c389832427a434bd8e2d6ecc75
Fixes: 1051c4208834c22d344dd92d25addfd8e5d95993
Related: OS#3355

include/osmocom/msc/vty.h
@@ -31,6 +31,4 @@ void msc_vty_init(struct gsm_network *msc_network);
 
 struct gsm_network *gsmnet_from_vty(struct vty *vty);
 
-extern bool msc_parsing_config_file;
-
 #endif

src/libmsc/msc_vty.c
@@ -466,7 +466,7 @@ DEFUN(cfg_msc_ipa_name,
       "This name is used for GSUP routing and must be set if more than one MSC is connected to the HLR. "
       "The default is 'MSC-00-00-00-00-00-00'.\n")
 {
-	if (!msc_parsing_config_file) {
+	if (vty->type != VTY_FILE) {
 		vty_out(vty, "The IPA name cannot be changed at run-time; "
 			"It can only be set in the configuraton file.%s", VTY_NEWLINE);
 		return CMD_WARNING;

src/osmo-msc/msc_main.c
@@ -515,8 +515,6 @@ extern void *tall_gsms_ctx;
 extern void *tall_call_ctx;
 extern void *tall_trans_ctx;
 
-bool msc_parsing_config_file = false;
-
 int main(int argc, char **argv)
 {
 	int rc;
@@ -569,9 +567,7 @@ int main(int argc, char **argv)
 		return -1;
 #endif
 
-	msc_parsing_config_file = true;
 	rc = vty_read_config_file(msc_cmdline_config.config_file, NULL);
-	msc_parsing_config_file = false;
 	if (rc < 0) {
 		LOGP(DMSC, LOGL_FATAL, "Failed to parse the config file: '%s'\n",
 		     msc_cmdline_config.config_file);