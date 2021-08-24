osmocom/osmo-msc

commit 0803d88d9aa6ff36896fbde218018db3bea61dc7

vlr_sgs: fix SGs IMSI detech from non EPS services
When the subscriber is detached from non EPS services while the
SGs-association is not SGs-NULL, it needs to be removed from the VLR
database.

Change-Id: I575cf6036ad39468f590b2d57a06cd3512a4c31c
Related: OS#3614

src/libvlr/vlr_sgs.c
@@ -131,8 +131,15 @@ void vlr_sgs_imsi_detach(struct vlr_instance *vlr, const char *imsi, enum sgsap_
 {
 	struct vlr_subscr *vsub;
 	enum sgs_ue_fsm_event evt;
+
 	vsub = vlr_subscr_find_by_imsi(vlr, imsi);
 	if (!vsub)
+		return;
+
+	/* See also: 3GPP TS 29.118, 5.6.3 Procedures in the VLR: In case of
+	 * an implicit detach, we are supposed to check if the state of the
+	 * SGs-association, and only when it is not SGs-NULL, we may proceed. */
+	if (vsub->sgs_fsm->state == SGS_UE_ST_NULL && type == SGSAP_ID_NONEPS_T_IMPLICIT_UE_EPS_NONEPS)
 		return;
 
 	switch (type) {
@@ -153,6 +160,11 @@ void vlr_sgs_imsi_detach(struct vlr_instance *vlr, const char *imsi, enum sgsap_
 
 	osmo_fsm_inst_dispatch(vsub->sgs_fsm, evt, NULL);
 	vlr_subscr_put(vsub);
+
+	/* Detaching from non EPS services essentially means that the
+	 * subscriber is detached from 2G. In any case the VLR will
+	 * get rid of the subscriber. */
+	vlr_subscr_expire(vsub);
 }
 
 /*! Perform an SGs EPS detach.

commit 25b3ffb5f4cd2b2ef393b458096316272d7ee5b0

vlr_sgs.c: Fix missing use_count decrease in vlr_sgs_imsi_detach
it was recently observed in a pcap trace with gsmtap_log that the
use_count contained a "vlr_sgs_imsi_detach" item despite no related
message was seen near by. Further investigation shows that there's an
unbalanced get+put code path, introduced by an early return added to fix
another issue.

related: SYS#5337
Fixes: 0803d88d9aa6ff36896fbde218018db3bea61dc7
Change-Id: I91ae956e50fca2f4d0e1d145d60ccb0ebfb409e9

src/libvlr/vlr_sgs.c
@@ -148,8 +148,10 @@ void vlr_sgs_imsi_detach(struct vlr_instance *vlr, const char *imsi, enum sgsap_
 	/* See also: 3GPP TS 29.118, 5.6.3 Procedures in the VLR: In case of
 	 * an implicit detach, we are supposed to check if the state of the
 	 * SGs-association, and only when it is not SGs-NULL, we may proceed. */
-	if (vsub->sgs_fsm->state == SGS_UE_ST_NULL && type == SGSAP_ID_NONEPS_T_IMPLICIT_UE_EPS_NONEPS)
+	if (vsub->sgs_fsm->state == SGS_UE_ST_NULL && type == SGSAP_ID_NONEPS_T_IMPLICIT_UE_EPS_NONEPS) {
+		vlr_subscr_put(vsub, __func__);
 		return;
+	}
 
 	switch (type) {
 	case SGSAP_ID_NONEPS_T_EXPLICIT_UE_NONEPS: