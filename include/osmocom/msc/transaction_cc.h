9f898a86c0c7558e09cc3b68ca90a83c496eee3d
Oliver Smith <osmith@sysmocom.de>
2023-05-23 13:38:33 +0200

    codec_filter_set_ms_from_bc: prepare for CSD
    In all the places where codec_filter_ functions get called, for CSD we
    will need to filter the bearer services. Add a new
    transaction_cc.c file for functions that either combine the
    codec_filter_ function with logic for CSD and voice calls or just call
    the existing codec_filter function and a new csd_filter function.
    
    Start with moving codec_filter_set_ms_from_bc to this new file, it will
    be extended with a case for CSD in a future patch.
    
    Related: OS#4394
    Change-Id: If225f2a299ce6bc9ae35a17d6f591d889f49155e

---
+++ include/osmocom/msc/transaction_cc.h
@@ -0,0 +1,33 @@
+/* Filter/overlay codec and CSD bearer service selections for voice calls/CSD,
+ * across MS, RAN and CN limitations
+ *
+ * (C) 2023 by sysmocom - s.f.m.c. GmbH <info@sysmocom.de>
+ * All Rights Reserved
+ *
+ * Author: Oliver Smith
+ *
+ * SPDX-License-Identifier: AGPL-3.0+
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <osmocom/gsm/mncc.h>
+
+#include <osmocom/msc/codec_mapping.h>
+#include <osmocom/msc/transaction.h>
+#include <osmocom/msc/vlr.h>
+
+void trans_cc_filter_set_ms_from_bc(struct gsm_trans *trans, const struct gsm_mncc_bearer_cap *bcap);
---
+++ src/libmsc/transaction_cc.c
@@ -0,0 +1,43 @@
+/* Filter/overlay codec and CSD bearer service selections for voice calls/CSD,
+ * across MS, RAN and CN limitations
+ *
+ * (C) 2023 by sysmocom - s.f.m.c. GmbH <info@sysmocom.de>
+ * All Rights Reserved
+ *
+ * Author: Oliver Smith
+ *
+ * SPDX-License-Identifier: AGPL-3.0+
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <osmocom/msc/transaction_cc.h>
+
+void trans_cc_filter_set_ms_from_bc(struct gsm_trans *trans, const struct gsm_mncc_bearer_cap *bcap)
+{
+	trans->cc.codecs.ms = (struct sdp_audio_codecs){0};
+
+	if (!bcap)
+		return;
+
+	switch (bcap->transfer) {
+	case GSM48_BCAP_ITCAP_SPEECH:
+		sdp_audio_codecs_from_bearer_cap(&trans->cc.codecs.ms, bcap);
+		break;
+	default:
+		LOG_TRANS(trans, LOGL_ERROR, "Handling of information transfer capability %d not implemented\n",
+			  bcap->transfer);
+		break;
+	}
+}
