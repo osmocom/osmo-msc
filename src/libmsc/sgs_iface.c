ef98c9e4141bee76fd27ffa029d6dbceb38ece64
Philipp Maier <pmaier@sysmocom.de>
2026-08-13 16:51:59 +0200

    sgs_iface: guard against over-long MME name IEs.
    The function decode_mme_name decodes the MME name from the given
    tlv_parsed struct a buffer referenced by mme_name. Since the maximum
    length of the MME name is fixed, the length of the output buffer is
    also fixed (SGS_MME_NAME_LEN bytes + 1 byte string terminator).
    
    Unfortunately the function does not guard against over-long input.
    When the SGSAP_IE_MME_NAME IE is longer than SGS_MME_NAME_LEN, then
    either memcpy or osmo_apn_to_str may overflow the output buffer.
    
    Bug reported by: adam.bedard@gmail.com
    Related: OS#7058

--- src/libmsc/sgs_iface.c
+++ src/libmsc/sgs_iface.c
@@ -168,6 +168,10 @@ static int decode_mme_name(char *mme_name, const struct tlv_parsed *tp)
 	if (!mme_name_enc)
 		return -EINVAL;
 
+	/* do not accept IEs which exceed SGS_MME_NAME_LEN */
+	if (TLVP_LEN(tp, SGSAP_IE_MME_NAME) > SGS_MME_NAME_LEN)
+		return -EINVAL;
+
 	/* some implementations use FDQN format violating TS 29.118 9.3.14 */
 	if (!osmo_parse_mme_domain(&gummei, (const char *) mme_name_enc)) {
 		memcpy(mme_name, mme_name_enc, TLVP_LEN(tp, SGSAP_IE_MME_NAME));
